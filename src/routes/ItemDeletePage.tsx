5bb2c681542a71549acc749a144d1ca3fec34b82
AslanAb <xabda001@gmail.com>
2023-03-27 00:12:41 +0600

    2

--- src/routes/ItemDeletePage.tsx
+++ src/routes/ItemDeletePage.tsx
@@ -21,13 +21,14 @@ const ItemDeletePage = () => {
     }
     return (
         <div>
+            <p className="max-w-fit p-2 m-2 h-12 h-fit rounded-lg bg-blue-500 hover:-translate-y-1 hover:scale-101 hover:brightness-150 duration-300 m-auto text-2xl m-2 font-extrabold my-8">Удаление товара</p>
             {items.map((item) => (
-                <div key={item.id}>
-                    <p>{item.id}</p>
-                    <p>{item.name}</p>
-                    <p>{item.quantity}</p>
-                    <input type="number" onChange={(e) => setinputValue(e.target.valueAsNumber)} value={inputValue} />
-                    <button onClick={() => deleteBtn(item.id, item.quantity, inputValue, "Удаление", item.name, inputValue * (item.total_cost / item.quantity))}>delete</button>
+                <div key={item.id} className="max-w-fit p-2 m-2 h-12 h-fit rounded-lg bg-sky-200 hover:brightness-110 duration-300">
+                    <p>Наименование товара: {item.name}</p>
+                    <p>Количество товара на складе: {item.quantity}</p>
+                    <span>Количество для удаления: </span>
+                    <input type="number" onChange={(e) => setinputValue(e.target.valueAsNumber)} value={inputValue} className="max-w-fit p-1 m-2 h-12 h-fit rounded-lg border-2 border-blue-500/100"/>
+                    <button onClick={() => deleteBtn(item.id, item.quantity, inputValue, "Удаление", item.name, inputValue * (item.total_cost / item.quantity))} className="min-w-fit w-40 h-11 flex justify-center items-center bg-blue-600 rounded-xl hover:brightness-200 duration-300">delete</button>
                 </div>
             ))}
         </div>
--- src/routes/ItemSalePage.tsx
+++ src/routes/ItemSalePage.tsx
@@ -22,17 +22,17 @@ const ItemSalePage = () => {
     }
     return (
         <div>
-            <div>
-                Общий баланс {balance}
+            <div className="max-w-fit p-2 m-2 h-12 h-fit rounded-lg bg-red-500 hover:-translate-y-1 hover:scale-101 hover:brightness-150 duration-300">
+                Общий баланс: {balance}
             </div>
             {items.map((item) => (
-                <div key={item.id}>
-                    <p>{item.id}</p>
-                    <p>{item.name}</p>
-                    <p>{item.quantity}</p>
+                <div key={item.id} className="max-w-fit p-2 m-2 h-12 h-fit rounded-lg bg-red-200 hover:brightness-110 duration-300">
+                    <p>Наименование товара: {item.name}</p>
+                    <p>Количество товара на складе: {item.quantity}</p>
                     <p>Сумма от продажи: {inputValue ? inputValue * item.one_item_average_price : 0}</p>
-                    <input type="number" onChange={(e) => setinputValue(e.target.valueAsNumber)} value={inputValue} />
-                    <button onClick={() => saleBtn(item.id, item.quantity, inputValue, item.one_item_average_price * inputValue, "Продажа", item.name)}>Sale</button>
+                    <span>Количество для продажи: </span>
+                    <input type="number" onChange={(e) => setinputValue(e.target.valueAsNumber)} value={inputValue} className="max-w-fit p-1 m-2 h-12 h-fit rounded-lg border-2 border-blue-500/100"/>
+                    <button onClick={() => saleBtn(item.id, item.quantity, inputValue, item.one_item_average_price * inputValue, "Продажа", item.name)} className="min-w-fit w-40 h-11 flex justify-center items-center bg-red-600 rounded-xl hover:brightness-200 duration-300">Продать</button>
                 </div>
             ))}
         </div>
